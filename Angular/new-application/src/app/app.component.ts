ba034314b748423fbc95f26191497f4ca8474675
Alex Santos <alexsantosinformatica@gmail.com>
2022-07-30 08:18:34 -0300

    [Angular] - NgClass, NgStyle

---
+++ Angular/new-application/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent implements OnInit {
+  count = 0;
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+    let interval = setInterval(() => {
+      this.count++;
+      if (this.count === 10) {
+        clearInterval(interval)
+      }
+    }, 1000)
+
+  }
+}
